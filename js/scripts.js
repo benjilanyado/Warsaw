e4ac4b885aea4856876e5bbab96d455f78b23f15
Benji Lanyado <benjilanyado@gmail.com>
2013-12-03 16:55:15 +0000

    newer

--- js/scripts.js
+++ js/scripts.js
@@ -46,45 +46,45 @@ $(document).ready(function(){
 ////////////SLIDE 2 /////////
 
 
-var verticalimages2 = [	"0px", "-627px", "-1254px", "-1881px"];
-var lightboxCurrent2 = 0;
-function downclick2() {
-	lightboxCurrent2 += 1
- 	$(".verticalslidewrapper2").animate({"top": verticalimages2[lightboxCurrent2]}, 1000)
- 	$(".upbutton2").fadeIn()
- 	$(".backtotop2").fadeIn()
- 	if (lightboxCurrent2==(verticalimages2.length - 1)) {
- 		$(".downbutton2").fadeOut()
- 		$(".upbutton2").fadeIn()
+var verticalimages1 = [	"0px", "-627px", "-1254px", "-1881px", "-2508px", "-3135px", "-3762px", "-4389px", "-5016px", "-5643px", "-6265px"];
+var lightboxCurrent1 = 0;
+function downclick1() {
+	lightboxCurrent1 += 1
+ 	$(".verticalslidewrapper1").animate({"top": verticalimages1[lightboxCurrent1]}, 1000)
+ 	$(".upbutton1").fadeIn()
+ 	$(".backtotop1").fadeIn()
+ 	if (lightboxCurrent1==(verticalimages1.length - 1)) {
+ 		$(".downbutton1").fadeOut()
+ 		$(".upbutton1").fadeIn()
  	}
 }
 
-function upclick2() {
-	lightboxCurrent2 -= 1
- 	$(".verticalslidewrapper2").animate({"top": verticalimages2[lightboxCurrent2]}, 1000)
- 	$(".downbutton2").fadeIn()
- 	if (lightboxCurrent2==(verticalimages2.length - 4)) {  //NB  "-5" needs to be whatever length of array is
- 		$(".upbutton2").fadeOut()
- 		$(".backtotop2").fadeOut()
+function upclick1() {
+	lightboxCurrent1 -= 1
+ 	$(".verticalslidewrapper1").animate({"top": verticalimages1[lightboxCurrent1]}, 1000)
+ 	$(".downbutton1").fadeIn()
+ 	if (lightboxCurrent1==(verticalimages1.length - 10)) {  //NB  "-5" needs to be whatever length of array is
+ 		$(".upbutton1").fadeOut()
+ 		$(".backtotop1").fadeOut()
  	}
 }
 
-$(".downbutton2").click(function(event) {      
+$(".downbutton1").click(function(event) {      
 	event.preventDefault();
-	downclick2()
+	downclick1()
 });
 
-$(".upbutton2").click(function(event) {   	
+$(".upbutton1").click(function(event) {   	
 	event.preventDefault();
-	upclick2()
+	upclick1()
 });
 
-$(".backtotop2").click(function(event) {   	
+$(".backtotop1").click(function(event) {   	
 	event.preventDefault();
-	$(".verticalslidewrapper2").animate({"top": "0px"}, 1000)
+	$(".verticalslidewrapper1").animate({"top": "0px"}, 1000)
 	lightboxCurrent2 = 0;
-	$(".downbutton2").fadeIn()
-	$(".backtotop2").fadeOut()
+	$(".downbutton1").fadeIn()
+	$(".backtotop1").fadeOut()
 });
 
 ////SLIDE2 ///
