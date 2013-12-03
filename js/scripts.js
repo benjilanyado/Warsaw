955295ca447f664e799c40ba2e7fd6b2c0cf1b46
Benji Lanyado <benjilanyado@gmail.com>
2013-12-03 15:36:02 +0000

    update

--- js/scripts.js
+++ js/scripts.js
@@ -91,18 +91,75 @@ $(".backtotop2").click(function(event) {
 
 var boxs = [
     {
-        selector: '#cover1',
-        headline: 'Bold Bean Cafe',
-        copy: "First stop of the day - <a target='_blank' href='http://www.boldbeancoffee.com/pages/cafe'>Bold Bean Cafe</a>. Thanks to <a target='_blank' href='https://twitter.com/pricesquire'>@pricesquire</a> for the tip. My Americano was very nice, as was the the breadline pie I had with it. It's quite a hip place.",
-        address: "• <a target='_blank' href='http://www.boldbeancoffee.com/pages/cafe'>boldbeancoffee.com</a>, 869 Stockton Street, Jacksonville<br/><br/>• Listen to <a target='blank' href='http://www.guardian.co.uk/travel/2013/apr/25/sweat-records-florida-road-trip-spotify-playlist'>Sweat Records' Florida road trip playlist</a>",
-        content: '<img src="images/boldbean.jpg">'
+        selector: '.number1',
+        selector2: '.mapplace1',
+        headline: 'Tragutta Park',
+        copy: "The park still contains the stadium where the football match that is featured in STARS took place. This game is known as “the last game” as it took place four days before the Germany invasion.  ",
+        address: "",
+        content: '<img src="images/tragulla.jpg">'
     },
 
     {
-        selector: '#cover2',
-        headline: 'Airstream ranch',
-        copy: 'On the way west along the I-4, I pulled in at Airstream Ranch.  Eight trailers/caravans shoved in the ground, lengthways. Frank and Dorothy Bates erected them in 2007. In 2010,<a target="_blank" href="http://www.tampabay.com/news/humaninterest/now-legit-as-art-airstream-ranch-will-honk-its-horn/1095257"> after a legal battle</a>, a judge ruled they could keep them up.',
-        content: '<img src="images/airstreams.jpg">',
+        selector: '.number2',
+        headline: 'Warsaw Zoo',
+        selector2: '.mapplace2',
+        copy: 'In pre-war Warsaw the zoo was one the largest in Europe and the city’s most popular visitor attraction. Although it was almost completely destroyed during the war, it was rebuilt and is now even more popular.',
+        content: '<img src="images/zoo.jpg">',
+        address: ''
+    },
+    {
+        selector: '.number3',
+        selector2: '.mapplace3',
+        headline: 'Sztuka Café',
+        copy: 'The Sztuka (Arts) Café was a popular meeting place. Despite the ever worsening conditions inside the ghetto many cultural events took place. The café was one of the venues where many of Warsaw’s most talented musicians, such as Wladyslaw Szpilman, performed. ',
+        content: '<img src="images/arts.jpg">',
+        address: ''
+    },
+    {
+        selector: '.number4',
+        selector2: '.mapplace4',
+        headline: 'Great Synagogue',
+        copy: 'The Great Synagogue’s large green dome dominated the Warsaw skyline. It was a spectacularly ornate building which could accommodate almost 2500 people. It’s destruction, in May 1943 was seen by the Nazis to symbolise their final “triumph” over Warsaw’s Jews.',
+        content: '<img src="images/synagogue.jpg">',
+        address: ''
+    },
+    {
+        selector: '.number5',
+        selector2: '.mapplace5',
+        headline: 'Pilsudski Square',
+        copy: 'Pilsudski Square is Warsaw’s traditional venue for large military displays.  After Hitler used it for his Victory Parade in September 1939 it was temporarily renamed “Adolf Hitler Platz”. ',
+        content: '<img src="images/square.jpg">',
+        address: ''
+    },
+    {
+        selector: '.number6',
+        selector2: '.mapplace6',
+        headline: 'Korczak Orphanage',
+        copy: 'Janusz Korczak helped to design the building  that housed his Jewish orphanage. It thrived there from 1912 to 1940 when it was forced to move within the ghetto walls. ',
+        content: '<img src="images/orphan1.jpg">',
+        address: ''
+    },
+    {
+        selector: '.number7',
+        selector2: '.mapplace7',
+        headline: 'Ghetto Orphanage',
+        copy: 'In 1940 Korczak was forced to relocate his orphanage to 33 Chlodna Street. There are no surviving images of the building but it was located very close to the wooden footbridge that was built to connect the “small” and “large” sections of the  ghetto in January 1942.',
+        content: '<img src="images/orphan2.jpg">',
+        address: ''
+    },
+    {
+        selector: '.number8',
+        selector2: '.mapplace8',
+        headline: 'Jewish Cemetery',
+        copy: 'The Jewish Cemetery on Okopowa Street opened in 1806 and has over 200,000 marked graves. There is a memorial to Janusz Korczak and the vast number of child victims. Simon Tenenbaum (Marcus’ father in STARS) is buried there; I was honoured to be able to pay my respects at his graveside.',
+        content: '<img src="images/grave.jpg">',
+        address: ''
+    },
+    {
+        selector: '#cover9',
+        headline: '',
+        copy: '<div class="mapplace mapplace5">  <b>5. Pilsudski Square</b> </div> <div class="mapplace mapplace6">  <b>6. Korczak Orphanage</b>  </div><div class="mapplace mapplace7"> <b>7. Ghetto Orphanage </b></div>  <div class="mapplace mapplace8"> <b>8. Jewish Cemetery </b> </div>  ',
+        content: '<div class="mapplace mapplace1"><b>1. Tragutta Park </b> </div> <div class="mapplace mapplace2">  <b>2. Warsaw Zoo</b>  </div>   <div class="mapplace mapplace3"> <b>3. Sztuka Café</b> </div> <div class="mapplace mapplace4"> <b>4. Great Synagogue</b></div>',
         address: ''
     }
 
@@ -126,6 +183,8 @@ function processPlaceClick(box) {
         $(box.selector).on('click', function(e){
             e.preventDefault();
             processPlaceClick(box);
+            $(".number").removeClass("selected")
+            $(box.selector).addClass("selected")
         });
     });
 
@@ -143,10 +202,24 @@ function processPlaceClick(box) {
       $("#address").html('')
        $(".dayofweek").fadeIn();
 
-    })
 
+    })
   });
 
+
+  $("#introtowarsaw a").click(function(event) {        
+
+    event.preventDefault();
+    $("#introtowarsaw").fadeOut()
+
+    })
+
+    $("#cover10").click(function(event) {        
+
+    event.preventDefault();
+    $("#introtowarsaw").fadeIn()
+
+    })
 
 
 
